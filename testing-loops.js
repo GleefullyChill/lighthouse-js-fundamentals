9d41249cf9a322121783a50b9bf10f6ba8264a0d
GledfullyChill <stephan.lofstrom@gmail.com>
2021-07-07 15:32:20 -0600

    working with loops

---
+++ testing-loops.js
@@ -0,0 +1,10 @@
+for (let repeat =100; repeat != 201; repeat++) {
+  
+  if (repeat % 3 === 0);{
+    console.log("loopy");
+  }   else if (repeat % 4 === 0){
+      console.log("lighthouse");
+  }   else {
+      console.log(repeat);
+  }
+}
